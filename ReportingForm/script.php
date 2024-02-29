49ff4ec43aaad3f54368e0b3f6dc07104104333c
Shivam Kumar <skshivam466@gmail.com>
2024-02-29 20:34:16 +0545

    ReportingForm completed

--- ReportingForm/script.php
+++ ReportingForm/script.php
@@ -4,6 +4,22 @@ $user = "root";
 $password = "";
 $db = "hiking";
 
+$conn = new mysqli("$host", "$user", "$password", "$db");
 
+if($conn->connect_error === True){
+    echo "Error Connecting the database". $conn->connect_errno;
+}
 
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $species = $_REQUEST['Species'];
+    $details = $_REQUEST['Details'];
+    $type = $_REQUEST['Type'];
+    $event = $_REQUEST['Ongoings'];
+}
+
+$sql = "INSERT INTO reports (SPECIES, DETAILS, TYPE, ONGOINGS) VALUES ('$species', '$details', '$type', '$event')";
+
+if($conn->query($sql)){
+    echo "Reported successfully";
+}
 ?>
