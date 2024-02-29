c7a06e2851b6a8b8313badaf6097d8f895d91870
Shivam Kumar <skshivam466@gmail.com>
2024-02-29 22:58:16 +0545

    ndkjfalkj

--- Authority/index.php
+++ Authority/index.php
@@ -17,7 +17,39 @@
     <input type="date" name="day" required>
         <button type="submit" onclick="submit()">Report action</button>
     </form>
-</div>
+
+    
+
+<br><br><br>
+
 
 </div>
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "hiking";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if($conn->connect_error === True){
+    echo "Error Connecting the database". $conn->connect_errno;
+}
+
+$sql = "SELECT SPECIES, DETAILS, TYPE, ONGOINGS FROM reports";
+
+$result = $conn->query($sql);
+
+if($result->num_rows>0){
+    echo "<table><tr><th>SPECIES</th><th>DETAILS</th><th>TYPE</th><th>ONGOINGS</th>";
+    while($rows=$result->fetch_assoc()){
+        echo "<tr><td>". $rows["SPECIES"]. "</td><td>". $rows["DETAILS"]. "</td><td>". $rows["TYPE"]."</td><td>". $rows["ONGOINGS"] . "</td></table>";
+    } 
+}
+
+?>
+
+</div>
+
 </body>
