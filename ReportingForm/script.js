0ba7037044095518faeb397cee0d01a06b1583b6
Shivam Kumar <skshivam466@gmail.com>
2024-02-29 23:29:17 +0545

    email lakdf

--- ReportingForm/script.js
+++ ReportingForm/script.js
@@ -1,3 +1,9 @@
 function submit(){
-    
+    var Species = document.forms[0].Species.value;
+    var Details = document.forms[0].Details.value;
+    var Type = document.forms[0].Type.checked;
+    var Ongoings = document.forms[0].Ongoings.value;
+    if(Species == null || Details == null || Type == false || Ongoings == null){
+
+    }
 }
